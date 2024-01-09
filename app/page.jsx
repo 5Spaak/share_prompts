63b64a23133a6ef85bcf976155b5faf9a70cab6b
Spaak <spaakjobrighngoma@gmail.com>
2024-01-09 18:02:10 +0100

    some changing in home page

--- app/page.jsx
+++ app/page.jsx
@@ -7,9 +7,13 @@ const Home = () => {
     <section className="w-full flex-center flex-col">
         <h1 className="head_text test-center" >
             Discover & Share
-            <br className="max-md:hidden"/>
-            <span className="orange_gradient text-center">AI-Powered Prompts</span>
         </h1>
+        <br className="max-md:hidden"/>
+        <h2 className="head_text test-center">
+        
+            <span className="text-center orange_gradient ">AI-Powered Prompts</span>
+
+        </h2> 
         <p className="desc text-center">
             Promptopia is an open-source AI prompting tool for modern world discover, create and share creative prompts
         </p>
